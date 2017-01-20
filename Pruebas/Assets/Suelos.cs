cb4483e7efc2fffcd3de5d14eb8737964092c9df
FranLG100 <fralg100@gmail.com>
2017-01-20 17:08:00 +0100

    Script Suelos
    Para que los monos no atraviesen el suelo al subir en ascensor. Aparecen
    una vez ya han subido.

---
+++ Pruebas/Assets/Suelos.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Suelos : MonoBehaviour {
+
+	[SerializeField]
+	GameObject cristal;
+	[SerializeField]
+	GameObject sueloGrande;
+	[SerializeField]
+	GameObject sueloPequeño;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.CompareTag("Grande")){
+			cristal.SetActive(true);
+			sueloGrande.SetActive(true);
+			sueloPequeño.SetActive(true);
+		}
+	}
+}
+
