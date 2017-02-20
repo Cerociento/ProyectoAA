60a2bca3a6f0185d052a692b5286a90ceb9db339
Cerociento <cerociento00@gmail.com>
2017-02-20 18:50:56 +0100

    Subida semana pasada.

--- Pruebas/Assets/Definitivo/Script/Player/Camara.cs
+++ Pruebas/Assets/Definitivo/Script/Player/Camara.cs
@@ -5,61 +5,68 @@ public class Camara : MonoBehaviour
 {
 
     public static Transform Target;
+    Transform LugarCamara;
+
+    [SerializeField]
+    Transform LugarPequenio;
+    [SerializeField]
+    Transform LugarGrande;
 
     [SerializeField]
     Vector3 sitioCamara;
 
     [SerializeField]
     Transform Grande;
-
     [SerializeField]
     Transform Pequeño;
 
+    [Range(1,10)]
     [SerializeField]
-    float reasignado;
-
+    float velocidad;
 
     void Start()
     {
         Target = Pequeño;
-        transform.localPosition = sitioCamara;
+        LugarCamara = LugarPequenio;
 		Grande.gameObject.GetComponent<Movimiento_Grande>().enabled = false;
 		Grande.gameObject.GetComponent<AudioListener>().enabled=false;
     }
 
-
     void Update()
-    {    
+    {
+
+       // transform.LookAt(Target);
+
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             Target = Pequeño;
-            transform.parent = Pequeño;
-            transform.localPosition = sitioCamara;
-            transform.localEulerAngles = Vector3.zero;
+            LugarCamara = LugarPequenio;
+            //transform.localPosition = sitioCamara;
+            //transform.localEulerAngles = Vector3.zero;
             Grande.gameObject.GetComponent<Movimiento_Grande>().enabled = false;
 			Grande.gameObject.GetComponent<AudioListener>().enabled=false;
             Pequeño.gameObject.GetComponent<Movimiento_Pequeño>().enabled = true;
 			Pequeño.gameObject.GetComponent<AudioListener>().enabled=true;
             Pausa.nPersonaje = 1;
         }
+
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            Target = Grande;            
-            transform.parent = Grande;
-            transform.localPosition = sitioCamara;
-            transform.localEulerAngles = Vector3.zero;
+            Target = Grande;
+            LugarCamara = LugarGrande;        
+            //transform.localPosition = sitioCamara;
+            //transform.localEulerAngles = Vector3.zero;
             Grande.gameObject.GetComponent<Movimiento_Grande>().enabled = true;
 			Grande.gameObject.GetComponent<AudioListener>().enabled=true;
             Pequeño.gameObject.GetComponent<Movimiento_Pequeño>().enabled = false;
 			Pequeño.gameObject.GetComponent<AudioListener>().enabled=false;
             Pausa.nPersonaje = 2;
-
         }
-
-
     }
+
     void LateUpdate()
     {
-        //transform.position = Target.position + sitioCamara;
+        transform.position = Vector3.Lerp (transform.position, LugarCamara.position, Time.deltaTime * velocidad);
+        transform.LookAt(Target.position + sitioCamara);
     }
 }
